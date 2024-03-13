bc23e29cf629343af1284d431d5d419735bbd93d
Pradeep Vanga <pradeepreddy.manu.iitkgp@gmail.com>
2024-03-13 09:47:28 +0530

    handle noop case

--- src/meritindia/daily_generation.py
+++ src/meritindia/daily_generation.py
@@ -10,13 +10,16 @@ proxy_url = os.getenv("PROXY_URL")
 
 
 request_inputs = list(dgh.get_request_inputs())
-req_body = {"type": "daily_state_generation", "inputs": request_inputs[:2]}
-res = requests.request("POST", proxy_url, json=req_body, timeout=60)
-try:
-    rows = res.json()["data"]
-except Exception:
-    print(f"Failed to fetch data for {req_body}")
-    print(res.text)
-    raise Exception("Failed to fetch data")
-dgh.save_data(rows, output_dir)
-dgh.save_tracking_data(rows)
+if request_inputs:
+    req_body = {"type": "daily_state_generation", "inputs": request_inputs[:2]}
+    res = requests.request("POST", proxy_url, json=req_body, timeout=60)
+    try:
+        rows = res.json()["data"]
+    except Exception:
+        print(f"Failed to fetch data for {req_body}")
+        print(res.text)
+        raise Exception("Failed to fetch data")
+    dgh.save_data(rows, output_dir)
+    dgh.save_tracking_data(rows)
+else:
+    print("Nothing to get")
