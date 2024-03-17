bf4beb44ceaec491bb0b48edd1e7258885acce30
Pradeep Vanga <pradeepreddy.manu.iitkgp@gmail.com>
2024-03-17 07:24:02 +0530

    Batch daily generation processing as the requests are timing out

--- src/meritindia/daily_generation.py
+++ src/meritindia/daily_generation.py
@@ -18,17 +18,23 @@ assert data_type in [
 
 request_inputs = list(dgh.get_request_inputs(data_type))
 assert len(request_inputs) < 500, "Too many inputs"
+
 if request_inputs:
-    req_body = {"type": data_type, "inputs": request_inputs}
-    print(json.dumps(req_body))
-    res = requests.request("POST", proxy_url, json=req_body, timeout=120)
-    try:
-        rows = res.json()["data"]
-    except Exception:
-        print(f"Failed to fetch data for {req_body}")
-        print(res.text)
-        raise Exception("Failed to fetch data")
-    dgh.save_data(data_type, rows)
-    dgh.update_tracking_metadata(data_type, rows)
+    batch_size = 20
+    num_batches = (len(request_inputs) + batch_size - 1) // batch_size
+
+    for i in range(num_batches):
+        batch_inputs = request_inputs[i * batch_size : (i + 1) * batch_size]
+        req_body = {"type": data_type, "inputs": batch_inputs}
+        print(json.dumps(req_body))
+        res = requests.request("POST", proxy_url, json=req_body, timeout=120)
+        try:
+            rows = res.json()["data"]
+        except Exception:
+            print(f"Failed to fetch data for {req_body}")
+            print(res.text)
+            raise Exception("Failed to fetch data")
+        dgh.save_data(data_type, rows)
+        dgh.update_tracking_metadata(data_type, rows)
 else:
     print("Nothing to get")
